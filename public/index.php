5efa44dff7d35dcd9b422b334d167917ddfbd1fe
Clément Grenier--Desrousseaux <clement.grenier.desrousseaux@gmail.com>
2022-07-08 16:38:38 +0200

    Router terminé

--- public/index.php
+++ public/index.php
@@ -1,7 +1,13 @@
 <?php
 
-
+use Router\Router;
 
 require('../vendor/autoload.php');
 
-echo $_GET['url'];
+$router = new Router($_GET['url']);
+
+$router->get('/', 'App\Controller\BlogController@index');
+$router->get('/articles', 'App\Controller\BlogController@listeArticles');
+$router->get('/article/:id', 'App\Controller\BlogController@show');
+
+$router->run();
