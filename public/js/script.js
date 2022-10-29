aebeef9d79cb2a65254fe5035fbbdcb76e2c7c0d
Clément Grenier--Desrousseaux <clement.grenier.desrousseaux@gmail.com>
2022-10-30 01:07:23 +0200

    Responsive -> admin index + nav

---
+++ public/js/script.js
@@ -0,0 +1,9 @@
+function navRepsonsive() {
+    if (document.getElementById('admin-nav').style.display === "none") {
+        document.getElementById('admin-nav').style.display = "block";
+    }
+
+    else if (document.getElementById('admin-nav').style.display === "block") {
+        document.getElementById('admin-nav').style.display = "none";
+    }
+}
